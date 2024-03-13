d3b2a9fa3c273117aa90f4716f3534f48e1314e0
AsmaShaik-514 <163107419+AsmaShaik-514@users.noreply.github.com>
2024-03-13 16:31:15 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SecondThirdLargestSum {
+    public static void main(String[] args) {
+        int[] values = {10, 51, 53, 39, 47, 44, 21};
+
+        int sum = findSecondThirdLargestSum(values);
+        System.out.println("Sum of 2nd and 3rd largest numbers: " + sum);
+    }
+
+    public static int findSecondThirdLargestSum(int[] arr) {
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        if (n < 2) {
+            System.out.println("Array should contain at least 2 elements.");
+            return 0;
+        }
+
+        return arr[n - 2] + arr[n - 3];
+    }
+}
